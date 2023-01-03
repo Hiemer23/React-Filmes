0292542d0229b69c5d7add58dfb6368d6d2267c0
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-03 16:55:18 -0300

    Ajustado problema com .env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+  }
+  console.log(moviesURL)
+  console.log(apiKey)
   return (
     <div>Home</div>
   )
