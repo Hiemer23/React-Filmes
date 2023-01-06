3cffcc86120bbba35fff91f4ebabf771bf8d2748
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-06 08:22:16 -0300

    Adição do Movie e Search

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { BsGraphUP, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill } from 'react-icons/bs'
+import './Movie.css'
+import MovieCard from "../components/MovieCard"
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
+  const { id } = useParams()
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setMovie(data)
+  }
+
+  useEffect(() => {
+    const movieURL = `${moviesURL}${id}?${apiKey}`
+    getMovie(movieURL)
+  }, [])
+
   return (
-    <div>Movie</div>
+    <div>{movie && <>{movie.title}</>}</div>
   )
 }
 
