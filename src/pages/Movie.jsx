010c00a8d9de92a574a0f19a23b7c830c9219ec8
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-06 11:42:02 -0300

    Término do projeto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { BsGraphUP, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill } from 'react-icons/bs'
+import { BsGraphUp, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill } from 'react-icons/bs'
 import './Movie.css'
 import MovieCard from "../components/MovieCard"
 const moviesURL = import.meta.env.VITE_API;
@@ -21,8 +21,42 @@ const Movie = () => {
     getMovie(movieURL)
   }, [])
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
+    })
+  }
+
   return (
-    <div>{movie && <>{movie.title}</>}</div>
+    <div className="movie-page">{movie && <>
+      <MovieCard movie={movie} showLink={false} />
+      <p className="tagline">{movie.tagline}</p>
+      <div className="info">
+        <h3>
+          <BsWallet2 /> Orçamento
+        </h3>
+        <p>{formatCurrency(movie.budget)}</p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsGraphUp /> Receita:
+        </h3>
+        <p>{formatCurrency(movie.revenue)}</p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsHourglassSplit /> Duração
+        </h3>
+        <p>{movie.runtime} minutos</p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsFillFileEarmarkTextFill /> Descrição
+        </h3>
+        <p className="description">{movie.overview}</p>
+      </div>
+    </>}</div>
   )
 }
 
